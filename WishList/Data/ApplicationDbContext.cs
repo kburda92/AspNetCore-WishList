2d65ff9f16b9ce961e7bed66b896ae20f3c8d9f4
kburda92 <krzysztofburda92@gmail.com>
2019-05-09 19:29:13 +0200

    line

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -10,6 +10,7 @@ namespace WishList.Data
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Item> Items { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
